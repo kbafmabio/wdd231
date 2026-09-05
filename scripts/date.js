4cec926c880ec1f9f555ced8cb00a508f0032697
felicitemabio-dev <bkouassi@byupathway.edu>
2026-09-05 22:50:01 +0000

    addeed change

---
+++ scripts/date.js
@@ -0,0 +1,8 @@
+const currentyear = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = today.getFullYear();
+
+const lastmodified = document.querySelector('#lastmodified');
+ldate.innerHTML = today.getfulldate();
