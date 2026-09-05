6ce4311790ef392477521c8a5333ea529d65b5b6
felicitemabio-dev <bkouassi@byupathway.edu>
2026-09-05 05:43:49 +0000

    added some code

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,14 @@
-// scor
+// declaration variable
 const navButton = document.querySelector('#nav-button');
+const navlink = document.querySelector('#nav-bar');
+
+// javascript show
+
+
+
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
-})
+    navlink.classList.toggle('show');
+});
+// const navButton = document.querySelector('#nav-buttton');
 
