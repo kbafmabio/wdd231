7a2061a235ad35bc4ff421d18fc4371146414f33
felicitemabio-dev <bkouassi@byupathway.edu>
2026-09-07 01:38:34 +0000

    added code

--- scripts/date.js
+++ scripts/date.js
@@ -2,7 +2,7 @@ const currentyear = document.querySelector("#currentyear");
 
 const today = new Date();
 
-year.innerHTML = today.getFullYear();
+currentyear.innerHTML = today.getFullYear();
 
 const lastmodified = document.querySelector('#lastmodified');
 ldate.innerHTML = today.getfulldate();
