026e63ad6792e27c5054503953ec353d8c95790b
felicitemabio-dev <bkouassi@byupathway.edu>
2026-09-07 04:27:02 +0000

    added change

--- scripts/course.js
+++ scripts/course.js
@@ -86,8 +86,6 @@ courses.forEach(course => {
     courseElement.innerHTML = `<h2> ${course.subject} ${course.number} - ${course.title} </h2>
     <p> <strong> Credits:</strong> ${course.credits} </p>
     <p> <strong> Certificate:</strong> ${course.certificate} </p>
-    <p> <strong> Description:</strong> ${course.description} </p>
-    <p> <strong> Technoologie:</strong> ${course.technology.join(', ')} </p>
     <p> <strong> Completed:</strong> ${course.completed ? '✅ Yes' : '❌ No'} </p> `;
 
     coursescontainer.appendChild(courseElement);
