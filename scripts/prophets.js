5faca3cfebbfc57635012ebc58a2cbc102125e6e
felicitemabio-dev <bkouassi@byupathway.edu>
2026-09-09 09:09:19 +0000

    added code

---
+++ scripts/prophets.js
@@ -0,0 +1,37 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+async function getProphetData(url) {
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    displayProphets(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+
+    prophets.forEach((prophet) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        // Build the h2 content out to show the prophet's full name
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
+
+getProphetData(url);
