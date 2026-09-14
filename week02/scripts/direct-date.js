9efd519f0fdaa9f1e991ded0c14ade0422a56832
felicitemabio-dev <bkouassi@byupathway.edu>
2026-09-14 02:30:52 +0000

    added and changing style

--- week02/scripts/direct-date.js
+++ week02/scripts/direct-date.js
@@ -5,6 +5,14 @@ const today = new Date();
 currentyear.innerHTML = today.getFullYear();
 
 const lastmodified = document.querySelector('#lastmodified');
-alert(document.lastModified);
+// alert(document.lastModified);
 
-lastmodified.textContent = new Date(document.lastModified);
+//lastmodified.textContent = new Date(document.lastModified);
+const options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+		year: "numeric"
+	};
+    
+lastmodified.innerHTML = new Date().toLocaleDateString("en-US", Option);
