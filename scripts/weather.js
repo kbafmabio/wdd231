93cf8738d58409ba4b24b2414796302d69da5fdc
felicitemabio-dev <bkouassi@byupathway.edu>
2026-09-21 01:38:03 +0000

    added code

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,11 +2,16 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Trier&appid=YOUR_API_KEY&units=metric';
+const keyapi = "8ac4b49694de823806557c0e24d13c75";
+const lat = "49,756043";
+const lon = "6,628846";
+
+const myUrl = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${keyapi}&units=imperial`;
+
 
 async function apiFetch() {
     try {
-        const response = await fetch(url);
+        const response = await fetch(myUrl);
         if (response.ok) {
             const data = await response.json();
             console.log(data); // testing only
@@ -22,7 +27,7 @@ async function apiFetch() {
 apiFetch();
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data._____}&deg;F`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${______}.___`;
     let desc = data.weather[0].______;
     weatherIcon.setAttribute('___', _____);
