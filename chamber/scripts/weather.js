11da2fe77829e85927046fb6bddb1ead224b2ff9
felicitemabio-dev <bkouassi@byupathway.edu>
2026-09-21 05:48:43 +0000

    added some code

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -4,8 +4,9 @@ const description = document.querySelector('#description');
 const temperature = document.querySelector('#temperature');
 
 const myKey = "8ac4b49694de823806557c0e24d13c75";
-const myLat = "5,242445";
-const myLon = "-3,626984";
+const myLat = "5.242445";
+const myLon = "-3.626984";
+
 const myURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=${myKey}&units=imperial`;
 
 async function apiFetch() {
@@ -14,7 +15,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data); // testing only
-            // displayResults(data); // uncomment when ready
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -22,5 +23,16 @@ async function apiFetch() {
         console.log(error);
     }
 }
+
+function displayResults(data) {
+    console.log('hello')
+    myTown.innerHTML = data.name
+    description.innerHTML = data.weather[0].description
+    temperature.innerHTML = `${data.main.temp}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    myGraphic.setAttribute('SRC', iconsrc)
+    myGraphic.setAttribute('alt',)
+}
+
 
 apiFetch();
--- scripts/weather.js
+++ scripts/weather.js
@@ -1,10 +1,12 @@
+const { myURL } = require("../chamber/scripts/weather");
+
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 const keyapi = "8ac4b49694de823806557c0e24d13c75";
-const lat = "49,756043";
-const lon = "6,628846";
+const lat = "49.756043";
+const lon = "6.628846";
 
 const myUrl = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${keyapi}&units=imperial`;
 
@@ -15,7 +17,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data); // testing only
-            // displayResults(data); // uncomment when ready
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
